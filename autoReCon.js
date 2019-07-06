fdbe73af9139d11fc6f4e640f869cdc2fc123c21
vongola <vongola@vongoladeMacBook-Pro.local>
2019-07-06 11:43:13 +0800

    接口断线重连

---
+++ autoReCon.js
@@ -0,0 +1,62 @@
+const test = {
+  getSource () {
+    Service.get('/source').then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+  }
+};
+
+class Service {
+  // 请求次数和请求间隔
+  static timeGaps = [1, 10, 60];
+  // 用于记录正在重试的函数和当前函数的时间间隔
+  static requestGaps = {};
+  // 待重新请求接口集合
+  static requestMap = {};
+
+  // 获取数据
+  static get(path) {
+    // 保存整个请求
+    this.requestMap[path] = () => {
+      return fetch(path)
+      .then(res => {
+        // 数据请求成功，删除requestMap中的key
+        if (res.ok) return res.json();
+        this.clearRequestMap(path);
+      })
+      .catch(err => {
+        if (err.toString().indexOf('timeout') !== -1) {
+          // 网络故障，呼叫控制器进行断网重试
+          this.retryControl(path);
+        } else {
+          console.log(err)
+        }
+      })
+    }
+    return this.requestMap[path]();
+  }
+
+  // 重试控制
+  static retryControl(name) {
+    const self = this
+    let index = self.timeGaps.indexOf(self.requestGaps[name]);
+    if (index < self.timeGaps.length - 1) {
+      index += 1;
+      self.requestGaps[name] = self.timeGaps[index];
+      setTimeout(() => {
+        // 重新发送请求，清除时直接跳过
+        self.requestMap[name] && self.requestMap[name]();
+      }, 1000 * self.timeGaps[index]);
+    } else {
+      // 超过请求次数，不再请求
+      self.clearRequestMap(name);
+      alert('请检查网络')
+    }
+  }
+
+  static clearRequestMap(name) {
+    delete this.requestMap[name]
+    delete this.requestGaps[name]
+  }
+}
